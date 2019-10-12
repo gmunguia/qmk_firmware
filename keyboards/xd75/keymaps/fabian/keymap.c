97aef129d203ee8e8e002718d05df48ac4ca934e
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2019-10-12 21:56:13 +0200

    fix option in mac

--- keyboards/xd75/keymaps/fabian/keymap.c
+++ keyboards/xd75/keymaps/fabian/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { HPR_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
   { KC_CAPSLOCK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LBRC/*KC_QUOT*/ },
   { SFT_BSP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT },
-  { KC_LCTL, _______, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  _______, _______, _______, KC_SPC,  RAISE,   KC_RGUI, _______, _______, KC_RCTL }
+  { KC_LCTL, _______, KC_RALT, KC_LGUI, LOWER,   KC_SPC,  _______, _______, _______, KC_SPC,  RAISE,   KC_RGUI, _______, _______, KC_RCTL }
 },
 
 /* Lower
